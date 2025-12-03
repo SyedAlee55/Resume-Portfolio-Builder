fe7bec261d07a313c9f913daae0ead2850666272
SyedAlee55 <alisaman614@gmail.com>
2025-12-03 20:22:20 +0500

    Added nodemon lib to backend

--- backend/index.js
+++ backend/index.js
@@ -23,4 +23,4 @@ app.use('/api/resumes', require('./routes/resumes'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: ${'http://localhost:'}${PORT}`));
